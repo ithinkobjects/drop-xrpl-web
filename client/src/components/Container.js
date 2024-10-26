f6265c9326f725ca54390a62286b4d3c4b52660f
Marlon <marlonaesparza@protonmail.com>
2024-10-26 16:56:39 -0700

    updated app content container wwidth

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -6,13 +6,13 @@ const Container = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 90%;
+    width: 85%;
     height: auto;
     @media (max-width: 768px) {  /* Tablet */
       width: 80%;
     }
     @media (max-width: 480px) {  /* Mobile */
-      width: 70%;
+      width: 76%;
     }
 `;
 
